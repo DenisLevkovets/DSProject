77c53b75744ce0bf9fe8ce1072b7d32452390911
DenisLevkovets <levkovets_da@mail.ru>
2019-11-25 18:54:38 +0300

    Changes

--- app.py
+++ app.py
@@ -1,45 +1,126 @@
+import requests
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-counter = 0
-
-ips = []
+counter = 1
+ips = ['http://127.0.0.1:5566/', 'http://127.0.0.1:5577/', 'http://127.0.0.1:5588/']
+available_ips = []
 
 fs = {}
 fs['dir1'] = {}
 fs['dir1']['dir3'] = {}
 fs['dir1']['dir2'] = {}
-fs['dir1']['dir2']['filename.txt'] = [[1, 2], counter]  # ss and id
 
 
 @app.route('/file/create', methods=['GET'])
 def createFile():
-    path = request.json['path']
+    global counter
+    path = request.args.get('path')
     cur_dir, filename = search(path)
     if type(cur_dir) is dict:
-        cur_dir[filename] = [[1], counter]
-        print(fs)
-        return 'File was created'
+        try:
+            check = cur_dir[filename]
+            return {"res": "500"}
+        except KeyError:
+            cur_dir[filename] = [[], int(counter)]
+            counter += 1
+            return {'res': '200'}
     else:
-        return "Error"
+        return {'res': '404'}
 
 
 @app.route('/file/read', methods=['GET'])
 def readFile():
-    return 'File was readed'
+    path = request.args.get('path')
+    cur_dir, name = search(path)
+    ping()
+    try:
+        if type(cur_dir[name]) is list and len(available_ips) > 0:
+            return {"res": available_ips[0] + "get_file", 'id': int(cur_dir[name][1])}
+    except KeyError:
+        return {"res": "404"}
 
 
-@app.route('/file/write', methods=['POST'])
+@app.route('/file/write', methods=['GET'])
 def writeFile():
-    return 'File was written'
+    global counter
+    path = request.args.get('path')
+    cur_dir, filename = search(path)
+    ping()
+    if type(cur_dir) is dict and len(available_ips) > 0:
+        resp = {"res": available_ips[0] + 'upload', "id": int(counter)}
+        counter += 1
+        return resp
+    else:
+        return {"res": "404"}
+
+
+@app.route('/file/upload/finish', methods=['GET'])
+def uploadFinish():
+    path = request.args.get('path')
+    index = request.args.get('index')
+    cur_dir, name = search(path)
+    remote_addr = 'http://' + request.remote_addr + ":5566/"
+    cur_dir[name] = [[remote_addr], int(index)]
+    ips_to_sync = available_ips.copy()
+    ips_to_sync.pop(available_ips.index(remote_addr))
+    for ip in ips_to_sync:
+        response = requests.post(ip + 'sync_recv', data={'ip': str(remote_addr), 'index': index, 'filename': name, 'path': path})
+        if response.status_code == 400:
+            return {"res": "400"}
+        elif response.status_code == 200:
+            cur_dir[name][0].append(ip)
+    return {"res": "200"}
 
 
 @app.route('/file/delete', methods=['GET'])
 def deleteFile():
-    return 'File was deleted'
+    path = request.args.get('path')
+    cur_dir, name = search(path)
+    ping()
+    try:
+        print(available_ips, 'delete')
+        if type(cur_dir[name]) is list and len(available_ips) > 0:
+            return {"res": available_ips[0] + "remove", 'id': int(cur_dir[name][1]), 'status': '200'}
+        else:
+            return {"res": "500"}
+    except KeyError:
+        return {"res": "404"}
+
+
+@app.route('/file/delete/finish', methods=['GET'])
+def deleteFinish():
+    path = request.args.get('path')
+    cur_dir, name = search(path)
+    remote_addr = 'http://' + request.remote_addr + ":5566/"
+    ips_to_sync = available_ips.copy()
+    ips_to_sync.pop(available_ips.index(remote_addr))
+    print(ips_to_sync, 'deletesync')
+    try:
+        if type(cur_dir[name]) is list:
+            index = cur_dir[name][1]
+            print(1)
+            if len(ips_to_sync) >= 1:
+                print(2)
+                while len(ips_to_sync) != 0:
+                    print(3)
+                    for ip in ips_to_sync:
+                        print(4)
+                        response = requests.get(ip + 'sync_remove', params={'index': index})
+                        if response.status_code == 400:
+                            print('err')
+                            return {"res": "404"}
+                        elif response.status_code == 200:
+                            ips_to_sync.pop(ips_to_sync.index(ip))
+            cur_dir.pop(name)
+            return {"res": "200"}
+        else:
+            return {"res": "500"}
+    except KeyError:
+        return {"res": "404"}
 
 
 @app.route('/file/info', methods=['GET'])
@@ -49,75 +130,104 @@ def fileInfo():
 
 @app.route('/file/copy', methods=['GET'])
 def copyFile():
-    source = request.json['source']
-    destination = request.json['destination']
+    global counter
+    source = request.args.get('source')
+    destination = request.args.get('destination')
     cur_dir_s, name_s = search(source)
     cur_dir_d, name_d = search(destination)
-    print(fs)
     if name_s and name_d:
         try:
             cur_dir_d[name_d] = cur_dir_s[name_s].copy()
-            cur_dir_d[name_d]['id'] += 1
-            print(fs)
-            return 'File was copied'
+            cur_dir_d[name_d][1] = counter
+            counter += 1
+            return {"res": "200"}
         except KeyError:
-            return 'error'
+            return {"res": "400"}
     else:
-        return 'error'
+        return {"res": "400"}
 
 
 @app.route('/file/move', methods=['GET'])
 def moveFile():
-    source = request.json['source']
-    destination = request.json['destination']
+    source = request.args.get('source')
+    destination = request.args.get('destination')
     cur_dir_s, name_s = search(source)
     cur_dir_d, name_d = search(destination)
-    print(fs)
     if name_s and name_d:
         cur_dir_d[name_d] = cur_dir_s[name_s]
         cur_dir_s.pop(name_s)
-        print(fs)
-        return 'File was moved'
+        return {"res": "200"}
     else:
-        return 'error'
+        return {"res": "400"}
 
 
 @app.route('/init', methods=['GET'])
 def init():
-    return 'Initialization'
+    global fs, available_ips
+    fs = {}
+    for ip in ips:
+        try:
+            response = requests.get(ip + 'ping')
+            if response.status_code == 200:
+                available_ips.append(ip)
+            elif response.status_code == 400:
+                pass
+        except requests.exceptions.ConnectionError:
+            pass
+    return {"res": 'Initialization'}
 
 
 @app.route('/dir/read', methods=['GET'])
 def readDir():
     path = request.args.get('path')
     cur_dir, name = search(path)
-    return jsonify(str(list(cur_dir[name].keys())))
+    if len(path) == 0:
+        return jsonify({"res": str(list(cur_dir.keys()))})
+    else:
+        return jsonify({"res": str(list(cur_dir[name].keys()))})
 
 
 @app.route('/dir/make', methods=['GET'])
 def makeDir():
-    path = request.json['path']
+    path = request.args.get('path')
     cur_dir, name = search(path)
-    if name:
-        cur_dir[name] = {}
-        print(fs)
-        return '200'
-    else:
-        print(fs)
-        return '404'
+    try:
+        if type(cur_dir[name]) is dict:
+            return {'res': "500"}
+    except KeyError:
+        if name:
+            cur_dir[name] = {}
+            return {'res': "200"}
+        else:
+            return {'res': "404"}
 
 
 @app.route('/dir/delete', methods=['GET'])
 def deleteDir():
-    path = request.json['path']
+    path = request.args.get('path')
     cur_dir, name = search(path)
-    if name:
-        cur_dir.pop(name)
-        print(fs)
-        return '200'
-    else:
-        print(fs)
-        return '404'
+    try:
+        if type(cur_dir[name]) is dict:
+            cur_dir.pop(name)
+            return {"res": "200"}
+        else:
+            return {"res": "500"}
+    except KeyError:
+        return {"res": "404"}
+
+
+@app.route('/dir/open', methods=['GET'])
+def openDir():
+    path = request.args.get('path')
+    cur_dir, name = search(path)
+    print(cur_dir, name)
+    try:
+        if type(cur_dir[name]) is dict:
+            return {'res': '200'}
+        else:
+            return {'res': '500'}
+    except KeyError:
+        return {'res': '404'}
 
 
 def search(path):
@@ -126,25 +236,93 @@ def search(path):
     try:
         if len(dirs) > 1:
             cur_dir = fs[dirs[0]]
-            print(dirs)
             while len(dirs) > 2:
                 if not type(cur_dir) is dict:
                     return False, False
                 dirs = dirs[1:]
                 cur_dir = cur_dir[dirs[0]]
-                print(cur_dir)
             if type(cur_dir) is dict:
                 return cur_dir, filename
             return False, False
         else:
             return fs, filename
+
     except KeyError:
         return False, False
 
 
 def ping():
-    pass
+    global available_ips
+    new_available_ips = []
+    ips_to_sync = []
+    for ip in ips:
+        try:
+            response = requests.get(ip + 'ping')
+            if response.status_code == 200:
+                new_available_ips.append(ip)
+                if ip not in available_ips:
+                    ips_to_sync.append(ip)
+            elif response.status_code == 400:
+                pass
+        except requests.exceptions.ConnectionError:
+            pass
+    available_ips = new_available_ips
+    if len(ips_to_sync) > 0:
+        sync(ips_to_sync)
+
+
+def sync(ips):
+    for ip in ips:
+        response = requests.get(ip + 'file_list')
+        if response.status_code == 200:
+            indexes = response.json()
+            files_to_upload, files_to_delete = separate_from_dfs(ip, indexes)
+            for file in files_to_upload:
+                filename = file[1][:-1].split('/')[-1]
+                path = file[1][:-1]
+                index = file[0][1]
+                remote_addr = file[0][0][0]
+                cur_dir, name = search(path)
+                response = requests.post(ip + 'sync_recv', data={'ip': str(remote_addr), 'index': index, 'filename': filename, 'path': path})
+                if response.status_code == 400:
+                    return {"res": "400"}
+                elif response.status_code == 200:
+                    cur_dir[name][0].append(ip)
+            print(ip, files_to_delete)
+            for file in files_to_delete:
+                response = requests.get(ip + 'sync_remove', params={'index': file})
+                if response.status_code == 400:
+                    return {"res": "400"}
+
+
+def separate_from_dfs(ip, indexes):
+    dfs_files = dfs(fs, [])
+    dfs_indexes = []
+    files_to_upload = []
+    files_to_delete = []
+    for file in dfs_files:
+        dfs_indexes.append(file[0][1])
+        if ip not in file[0][0]:
+            files_to_upload.append(file)
+    for index in indexes:
+        if int(index) not in dfs_indexes:
+            files_to_delete.append(index)
+    return files_to_upload, files_to_delete
+
+
+def dfs(node, res, path=''):
+    if type(node) is list:
+        res.append([node, path])
+        return res
+    keys = node.keys()
+    if len(keys) != 0:
+        for key in keys:
+            dfs(node[key], res, path + key + '/')
+        return res
+    else:
+        return res
 
 
 if __name__ == '__main__':
+    init()
     app.run()
---
+++ ss.py
@@ -0,0 +1,105 @@
+import os
+from flask import Flask, request, send_from_directory, make_response, jsonify
+from werkzeug.utils import secure_filename
+from werkzeug.middleware.shared_data import SharedDataMiddleware
+import requests
+from flask_cors import CORS
+
+UPLOAD_FOLDER = './ss1'
+ns_ip = "http://127.0.0.1:5000"
+
+app = Flask(__name__)
+CORS(app)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 128 * 1024 * 1024
+app.secret_key = 'some secret key'
+
+app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+    '/ss1': app.config['UPLOAD_FOLDER']
+})
+
+
+@app.route('/sync_remove', methods=['GET'])
+def sync_remove():
+    try:
+        index = request.args.get('index')
+        path = os.path.join(app.config['UPLOAD_FOLDER'], index)
+        os.remove(path)
+        return make_response('succeed', 200)
+    except OSError:
+        return make_response('not succeed', 400)
+
+
+@app.route('/remove', methods=['GET'])
+def remove():
+    index = request.args.get('index')
+    path = os.path.join(app.config['UPLOAD_FOLDER'], index)
+    origin_path = request.args.get('path')
+    os.remove(path)
+    requests.get(ns_ip + "/file/delete/finish", params={'path': origin_path})
+    return make_response('succeed', 200)
+
+
+@app.route('/get_file', methods=['GET'])
+def uploaded_file():
+    index = request.args.get('index')
+    return send_from_directory(app.config['UPLOAD_FOLDER'], index)
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    file = request.files['file']
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if file.filename == '':
+        return make_response('error', 400)
+    index = request.form['index']
+    path = request.form['path']
+    if file:
+        index = secure_filename(index)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], index))
+        requests.get(ns_ip + "/file/upload/finish", params={'path': path,
+                                                            'index': index})
+        return make_response('succeed', 200)
+    return make_response('error', 400)
+
+
+@app.route('/ping')
+def ping():
+    return make_response(request.host, 200)
+
+
+@app.route('/file_list', methods=['GET'])
+def file_list():
+    files = [file for file in os.listdir(app.config['UPLOAD_FOLDER'])]
+    print(files)
+    return make_response(jsonify(files), 200)
+
+
+@app.route('/info')
+def info():
+    index = request.form['index']
+    size = round(os.path.getsize(os.path.join(app.config['UPLOAD_FOLDER'], index)) / (1024 * 1024), 2)
+    return make_response(jsonify(size), 200)
+
+
+@app.route('/sync_recv', methods=['POST'])
+def sync_recv():
+    ip = request.form['ip']
+    index = request.form['index']
+    path = request.form['path']
+    filename = request.form['filename']
+    if ip and index:
+        payload = {'index': index, 'filename': filename}
+        r = requests.get(ip + "get_file", params=payload)
+        file = open(os.path.join(app.config['UPLOAD_FOLDER'], index), "wb")
+        file.write(r.content)
+        file.close()
+        return make_response(jsonify(filename=filename,
+                                     path=path,
+                                     index=index), 200)
+    return make_response('error (ip or index is missing)', 400)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5566)
